ef81c466005abc9e7c1f9f3639db2189a7ea0836
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-04 09:15:21 -0500

    homepage react style, baby

---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage () {
+  return (
+    <div className="container">
+      <div className="overlay">
+      </div>
+    </div>
+  )
+}
+
+export default Homepage;
